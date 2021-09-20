fe9964438a640653e3fbeb7da53e1befa0cfebe9
Bruno Flores <bruno@brunoflores.com.br>
2021-09-20 22:48:21 +1000

    Prove "double".

--- Induction.v
+++ Induction.v
@@ -111,6 +111,20 @@ Proof.
       simpl.
       reflexivity.
 Qed.
+
+Fixpoint double (n : nat) :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+
+Lemma double_plus :
+  forall n, double n = n + n.
+Proof.
+  induction n.
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn. rewrite -> plus_n_Sm. reflexivity.
+Qed.
 Theorem plus_comm : forall n m : nat, n + m = m + n.
 Proof.
   induction n as [| n' IHn'].
