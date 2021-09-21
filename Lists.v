3f36829ac3560e49d934961df7533a661dc1300f
Bruno Flores <bruno@brunoflores.com.br>
2021-09-21 10:15:20 +1000

    Start Lists chapter.

---
+++ Lists.v
@@ -0,0 +1,47 @@
+From LF Require Export Induction.
+Module NatList.
+
+  Inductive natprod : Type :=
+    | pair (n1 n2 : nat).
+
+  Check (pair 3 5) : natprod.
+
+  Definition fst (p : natprod) : nat :=
+    match p with
+    | pair x y => x
+    end.
+
+  Definition snd (p : natprod) : nat :=
+    match p with
+    | pair x y => y
+    end.
+
+  Notation "( x , y )" := (pair x y).
+
+  Definition fst' (p : natprod) : nat :=
+    match p with
+    | (x, y) => x
+    end.
+
+  Definition snd' (p : natprod) : nat :=
+    match p with
+    | (x, y) => y
+    end.
+
+  Definition swap_pair (p : natprod) : natprod :=
+    match p with
+    | (x, y) => (y, x)
+    end.
+
+  Theorem surjective_pairing' :
+    forall (n m : nat), (n, m) = (fst (n, m), snd (n, m)).
+  Proof.
+    reflexivity. Qed.
+
+  Theorem surjective_pairing :
+    forall (p : natprod), p = (fst p, snd p).
+  Proof.
+    intros p.
+    destruct p as [n m].
+    simpl.
+    reflexivity. Qed.
