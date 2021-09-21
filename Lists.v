609718f5ba88800614750a0feb8794d5d04f49be
Bruno Flores <bruno@brunoflores.com.br>
2021-09-21 11:28:49 +1000

    Solve alternation of two lists.

--- Lists.v
+++ Lists.v
@@ -151,3 +151,31 @@ Module NatList.
     countoddmembers [1;0;3;1;4;5] = 4.
   Proof.
     simpl. reflexivity. Qed.
+
+  Fixpoint alternate (l1 l2 : natlist) : natlist :=
+    match l1, l2 with
+    | [], [] => []
+    | l1', [] => l1'
+    | [], l2' => l2'
+    | h1 :: t1, h2 :: t2 => h1 :: h2 :: alternate t1 t2
+    end.
+
+  Example test_alternate1:
+    alternate [1;2;3] [4;5;6] = [1;4;2;5;3;6].
+  Proof.
+    simpl. reflexivity. Qed.
+
+  Example test_alternate2:
+    alternate [1] [4;5;6] = [1;4;5;6].
+  Proof.
+    simpl. reflexivity. Qed.
+
+  Example test_alternate3:
+    alternate [1;2;3] [4] = [1;4;2;3].
+  Proof.
+    simpl. reflexivity. Qed.
+
+  Example test_alternate4:
+    alternate [] [20;30] = [20;30].
+  Proof.
+    simpl. reflexivity. Qed.
