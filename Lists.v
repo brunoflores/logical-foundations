2e2cbb84936539b2ccaef7c2aa32ac207f65a1b0
Bruno Flores <bruno@brunoflores.com.br>
2021-09-21 10:29:15 +1000

    Add some definitions for lists.

--- Lists.v
+++ Lists.v
@@ -61,3 +61,16 @@ Module NatList.
     destruct p as [n m].
     simpl.
     reflexivity. Qed.
+
+  Inductive natlist : Type :=
+    | nil
+    | cons (n : nat) (l : natlist).
+
+  Definition mylist := cons 1 (cons 2 (cons 3 nil)).
+
+  Notation "x :: l" := (cons x l)
+                       (at level 60, right associativity).
+
+  Notation "[  ]" := nil.
+
+  Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
