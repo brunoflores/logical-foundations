05d6bff36472cb26594b4f1d20f2274845bd696b
Bruno Flores <bruno@brunoflores.com.br>
2022-09-17 15:22:05 +1000

    Resolve exercise.

--- theories/Basics.v
+++ theories/Basics.v
@@ -345,6 +345,26 @@ Proof.
   reflexivity.
 Qed.
 
+Check mult_n_O.
+Theorem mult_n_0_m_0 : forall p q : nat,
+    (p * 0) + (q * 0) = 0.
+Proof.
+  intros p q.
+  rewrite <- mult_n_O.
+  rewrite <- mult_n_O.
+  reflexivity.
+Qed.
+
+Check mult_n_Sm.
+Theorem mult_n_1 : forall p : nat,
+    p * 1 = p.
+Proof.
+  intros p.
+  rewrite <- mult_n_Sm.
+  rewrite <- mult_n_O.
+  reflexivity.
+Qed.
+
 Theorem plus_1_neq_0_firsttry : forall n : nat,
   (n + 1) =? 0 = false.
 Proof.
