109359839c316295f5090864c1551e822d9ef1ab
Bruno Flores <bruno@brunoflores.com.br>
2022-09-18 13:43:52 +1000

    Continue.

--- theories/Poly.v
+++ theories/Poly.v
@@ -1,1 +1,22 @@
 From LF Require Export Lists.
+
+Inductive list (X:Type) : Type :=
+| nil : list X
+| cons : X -> list X -> list X.
+
+Check list : Type -> Type.
+Check (nil nat) : list nat.
+Check (cons nat 3 (nil nat)) : list nat.
+
+Check nil : forall X : Type, list X.
+Check cons : forall X : Type, X -> list X -> list X.
+
+Fixpoint repeat (X:Type) (x:X) (count:nat) : list X :=
+  match count with
+  | O => nil X
+  | S count' => cons X x (repeat X x count')
+  end.
+
+Example test_repeat1 :
+  repeat nat 4 2 = cons nat 4 (cons nat 4 (nil nat)).
+Proof. reflexivity. Qed.
