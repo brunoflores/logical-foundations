636eee29db340c002dee8ec62527dc1b0f7dfbed
Bruno Flores <bruno@brunoflores.com.br>
2022-09-18 13:57:53 +1000

    Continue.

--- theories/Poly.v
+++ theories/Poly.v
@@ -11,12 +11,45 @@ Check (cons nat 3 (nil nat)) : list nat.
 Check nil : forall X : Type, list X.
 Check cons : forall X : Type, X -> list X -> list X.
 
-Fixpoint repeat (X:Type) (x:X) (count:nat) : list X :=
+Arguments nil {X}.
+Arguments cons {X}.
+
+Fixpoint repeat {X:Type} (x:X) (count:nat) : list X :=
   match count with
-  | O => nil X
-  | S count' => cons X x (repeat X x count')
+  | O => nil
+  | S count' => cons x (repeat x count')
   end.
 
-Example test_repeat1 :
-  repeat nat 4 2 = cons nat 4 (cons nat 4 (nil nat)).
+Example test_repeat1 : repeat 4 2 = cons 4 (cons 4 nil).
+Proof. reflexivity. Qed.
+
+Fixpoint app {X:Type} (l1 l2 : list X) : list X :=
+  match l1 with
+  | nil => l2
+  | cons h t => cons h (app t l2)
+  end.
+
+Example test_app :
+  app (cons 1 (cons 2 nil)) (cons 3 nil) = (cons 1 (cons 2 (cons 3 nil))).
+Proof. reflexivity. Qed.
+
+Fixpoint rev {X:Type} (l : list X) : list X :=
+  match l with
+  | nil => nil
+  | cons h t => app (rev t) (cons h nil)
+  end.
+
+Example test_rev1 : rev (cons 1 (cons 2 nil)) = (cons 2 (cons 1 nil)).
+Proof. reflexivity. Qed.
+
+Example test_rev2 : rev (cons true nil) = cons true nil.
+Proof. reflexivity. Qed.
+
+Fixpoint length {X:Type} (l : list X) : nat :=
+  match l with
+  | nil => 0
+  | cons _ l' => S (length l')
+  end.
+
+Example test_length : length (cons 1 (cons 2 (cons 3 nil))) = 3.
 Proof. reflexivity. Qed.
