7931f322008f704a4bde66f04e918b963460a4bc
Bruno Flores <bruno@brunoflores.com.br>
2022-09-18 15:06:05 +1000

    Continue.

--- theories/Poly.v
+++ theories/Poly.v
@@ -164,3 +164,17 @@ Proof. reflexivity. Qed.
 
 Example test_nth_error3 : nth_error [true] 2 = None.
 Proof. reflexivity. Qed.
+
+Definition hd_error {X : Type} (l : list X) : option X :=
+  match l with
+  | [] => None
+  | h :: _ => Some h
+  end.
+
+Check @hd_error : forall X : Type, list X -> option X.
+
+Example test_hd_error1 : hd_error [1; 2] = Some 1.
+Proof. reflexivity. Qed.
+
+Example test_hd_error2 : hd_error [[1]; [2]] = Some [1].
+Proof. reflexivity. Qed.
