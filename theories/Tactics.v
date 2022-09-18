ef7efbb3168b5c9b4f3c66be52b0c732a7126e4a
Bruno Flores <bruno@brunoflores.com.br>
2022-09-18 18:24:32 +1000

    Continue.

--- theories/Tactics.v
+++ theories/Tactics.v
@@ -67,3 +67,34 @@ Example injection_ex3 : forall (X : Type) (x y z : X) (l j : list X),
   x = y.
 Proof. intros X x y z l j H1 H2.
        injection H1 as H3 H4. Abort.
+
+Theorem discriminate_ex1 : forall (n m : nat), false = true -> n = m.
+Proof. intros n m contra. discriminate contra. Qed.
+
+Theorem discriminate_ex2 : forall (n : nat), S n = O -> 2 + 2 = 5.
+Proof. intros n contra. discriminate contra. Qed.
+
+Example discriminate_ex3 :
+  forall (X : Type) (x y z : X) (l j : list X),
+  x :: y :: l = [] -> x = z.
+Proof. intros X x y z l j H. discriminate H. Qed.
+
+Theorem eqb_0_l : forall n, 0 =? n = true -> n = 0.
+Proof. intros n.
+       destruct n as [ | n'] eqn:E.
+       - (* n = 0 *)
+         intros H. reflexivity.
+       - (* n = S n' *)
+         simpl. intros H. discriminate H. Qed.
+
+Theorem f_equal : forall (A B : Type) (f : A -> B) (x y : A),
+  x = y -> f x = f y.
+Proof. intros A B f x y eq. rewrite -> eq. reflexivity. Qed.
+
+Theorem eq_implies_succ_equal : forall (n m : nat),
+  n = m -> S n = S m.
+Proof. intros n m H. apply f_equal. apply H. Qed.
+
+Theorem eq_implies_succ_equal' : forall (n m : nat),
+  n = m -> S n = S m.
+Proof. intros n m H. f_equal. apply H. Qed.
