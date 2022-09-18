7a41e02d10f22a7cad6992ce9bebe87bf1484703
Bruno Flores <bruno@brunoflores.com.br>
2022-09-18 16:41:35 +1000

    Continue.

--- theories/Poly.v
+++ theories/Poly.v
@@ -233,3 +233,19 @@ Theorem map_rev : forall (X Y : Type) (f : X -> Y) (l : list X),
 Proof. intros X Y f l. induction l.
        - reflexivity.
        - simpl. rewrite <- IHl. rewrite -> map_distr. reflexivity. Qed.
+
+Fixpoint flat_map {X Y : Type} (f : X -> list Y) (l : list X) : list Y :=
+  match l with
+  | [] => []
+  | h :: t => (f h) ++ (flat_map f t)
+  end.
+
+Example test_flat_map :
+  flat_map (fun n => [n;n;n]) [1;5;4] = [1;1;1;5;5;5;4;4;4].
+Proof. reflexivity. Qed.
+
+Definition option_map {X Y : Type} (f : X -> Y) (xo : option X) : option Y :=
+  match xo with
+  | None => None
+  | Some x => Some (f x)
+  end.
