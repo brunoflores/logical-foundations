df90bfaabc79ab7ccaad20718624bb0fdf7ef6bc
Bruno Flores <bruno@brunoflores.com.br>
2022-09-18 16:38:27 +1000

    Continue.

--- theories/Poly.v
+++ theories/Poly.v
@@ -212,3 +212,24 @@ Definition partition {X : Type}
 
 Example test_partition1 : partition oddb [1; 2; 3; 4; 5] = ([1; 3; 5], [2; 4]).
 Proof. reflexivity. Qed.
+
+Fixpoint map {X Y : Type} (f : X -> Y) (l : list X) : list Y :=
+  match l with
+  | [] => []
+  | h :: t => (f h) :: (map f t)
+  end.
+
+Example test_map : map oddb [2; 1; 2; 5] = [false; true; false; true].
+Proof. reflexivity. Qed.
+
+Lemma map_distr : forall (X Y : Type) (f : X -> Y) (l1 : list X) (l2 : list X),
+  map f (l1 ++ l2) = (map f l1) ++ (map f l2).
+Proof. intros X Y f l1 l2. induction l1.
+       - reflexivity.
+       - simpl. rewrite -> IHl1. reflexivity. Qed.
+
+Theorem map_rev : forall (X Y : Type) (f : X -> Y) (l : list X),
+  map f (rev l) = rev (map f l).
+Proof. intros X Y f l. induction l.
+       - reflexivity.
+       - simpl. rewrite <- IHl. rewrite -> map_distr. reflexivity. Qed.
